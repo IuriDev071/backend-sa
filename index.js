7ef0cf74088cc971e8f1e0cdcfaaae22f279955a
Iuri071 <iuri_matiello@estudante.sesisenai.org.br>
2021-11-30 20:50:34 -0300

    Post de Login -> FUNCIONANDO

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get('/users/', async (req, res) => {
 
     await database.sync();
 
-    res.json(await Usuario.findAll()).status(200);
+    res.json(await Usuario.findOne()).status(200);
 
 })
 
@@ -43,18 +43,23 @@ app.post('/servicos/', async (req, res) => {
         preco_peca, preco_mobra } = req.body;
 
     try {
-        const novoServico = await Servico.create({
-            produto,
-            data_entrada,
-            data_saida,
-            descricao,
-            preco_peca,
-            preco_mobra
-        })
+        if (produto != null && data_entrada != null
+            && data_saida != null && preco_mobra != null) {
+            const novoServico = await Servico.create({
+                produto,
+                data_entrada,
+                data_saida,
+                descricao,
+                preco_peca,
+                preco_mobra
+            })
 
-        novoServico.save(req.body)
+            novoServico.save(req.body)
 
-        res.json(req.body).status(201)
+            res.json(req.body).status(201)
+        } else {
+            res.json(error).status(400)
+        }
     } catch (error) {
         console.log(error)
     }
@@ -67,15 +72,49 @@ app.post('/usuarios/', async (req, res) => {
     const { nome, email, senha } = req.body;
 
     try {
-        const novoUsuario = await Usuario.create({
-            nome,
-            email,
-            senha
+        if (email != null && nome != null && senha != null) {
+            const novoUsuario = await Usuario.create({
+                nome,
+                email,
+                senha
+            })
+
+            novoUsuario.save(req.body)
+
+            res.json(req.body).status(201)
+        } else {
+            res.json(error).status(400)
+        }
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+// POST de Login
+app.post('/usuarios/login', async (req, res) => {
+    await database.sync();
+
+    let estaLogado = 0;
+
+    const { email, senha } = req.body;
+
+    try {
+
+        const user = await Usuario.findOne({
+            where: {
+                email: req.body.email
+            }
         })
 
-        novoUsuario.save(req.body)
-
-        res.json(req.body).status(201)
+        if (!user) {
+            estaLogado = 0;
+            res.json("Usuário não encontrado").status(404)
+            console.log(estaLogado)
+        } else {
+            estaLogado = 1;
+            res.json("Logado com sucesso").status(202)
+            console.log(estaLogado)
+        }
     } catch (error) {
         console.log(error)
     }
